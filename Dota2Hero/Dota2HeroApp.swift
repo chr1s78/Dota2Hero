c59e78922a1f1e554f31386ac65dc1950a1a5bf4
chr1s78 <ayahirano2014@126.com>
2021-08-08 13:36:44 +0800

    Initial Commit

---
+++ Dota2Hero/Dota2HeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dota2HeroApp.swift
+//  Dota2Hero
+//
+//  Created by Chr1s on 2021/8/8.
+//
+
+import SwiftUI
+
+@main
+struct Dota2HeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
